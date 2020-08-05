d886292745073bb585c7f94b210a9e2a7f97347e
Maximiliano Ibalborde <maximiliano_009@hotmail.com>
2020-08-05 17:34:20 -0300

    Add some images for homeScreen

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { View, StyleSheet, StatusBar } from 'react-native'
+import { View, StyleSheet, StatusBar, Image, Dimensions } from 'react-native'
 
 import colors from "../constants/colors"
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const screen = Dimensions.get("window")
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.blue,
-        flex: 1
+        flex: 1,
+        justifyContent: "center"
+    },
+    logoContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    logoBackground: {
+        width: screen.width * 0.45,
+        height: screen.height * 0.45, 
+    },
+    logo: {
+        position: "absolute",
+        width: screen.width * 0.25,
+        height: screen.height * 0.25, 
     },
 
 })
@@ -16,6 +31,18 @@ export default () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor={colors.blue} />
+      <View style={styles.logoContainer}>
+        <Image
+          source={require("../assets/images/background.png")}
+          style={styles.logoBackground}
+          resizeMode="contain"
+        />
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </View>
     </View>
   );
 };
